c61eb2ee4d5df9b56e8c87c7b67c694ef71153c3
Muhammed Shafeeque th <shafeeqsha06@gmail.com>
2024-09-27 09:52:48 +0530

    Time: 18 ms (87.23%) | Memory: 12.3 MB (84.11%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        return['FizzBuzz' if x % 15 == 0 else 'Fizz' if x % 3 == 0 else 'Buzz' if x % 5 == 0 else str(x) for x in range(1,n+1)]
+
+        
