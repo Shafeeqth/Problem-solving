99d5ddc8979ac04e93c2c269e0af275b6737cc6b
Muhammed Shafeeque th <shafeeqsha06@gmail.com>
2025-02-08 12:44:59 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (11.18%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.py
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.py
@@ -1,4 +1,4 @@
 class Solution:
     def sumOfUnique(self, nums: List[int]) -> int:
-        return sum(key for key, count in Counter(nums).items() if count < 2)
+        return sum(key for key, count in Counter(nums).items() if count == 1)
         
